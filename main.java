4e77885073a58843b88b5876474c56076413e238
mmjac10 <mmjac10@gmail.com>
2024-08-16 02:54:38 +0500

    Initial commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Helllo,This is a jenkins program");
+        System.out.println(new Date());
+
+    }
+}
