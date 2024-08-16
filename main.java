d7b89af6458d5ed8aa3d46b533c0e4d9fa2f373d
mmjac10 <mmjac10@gmail.com>
2024-08-16 20:29:27 +0500

    3rd commit
    3rd commit

--- main.java
+++ main.java
@@ -5,6 +5,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Helllo,This is a jenkins program");
         System.out.println("Welcome");
+        System.out.println("Another commit");
         System.out.println(new Date());
 
     }
